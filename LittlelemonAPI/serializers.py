edb78722a2118eb26cec5dbb8dd4149e8d163b7e
Kite Captain <incaptain703@gmail.com>
2023-07-19 08:23:20 +0300

    serialization

--- LittlelemonAPI/serializers.py
+++ LittlelemonAPI/serializers.py
@@ -8,10 +8,15 @@ class CategorySerializer(serializers.ModelSerializer):
         model = Category 
         fields = ['id', 'slug', 'title']
 
+# class MenuItemSerializer(serializers.HyperlinkedModelSerializer):
 class MenuItemSerializer(serializers.ModelSerializer):
     stock = serializers.IntegerField(source='inventory')
     price_after_tax = serializers.SerializerMethodField(method_name='calculate_tax')
-    category = CategorySerializer()
+    # category = CategorySerializer()
+    # category = serializers.HyperlinkedRelatedField(
+    #     querysery = Category.objects.all(),
+    #     view_name = 'category-detail'
+    # )
     class Meta:
         model = MenuItem 
         fields = [
@@ -22,6 +27,12 @@ class MenuItemSerializer(serializers.ModelSerializer):
             'price_after_tax',
             'category',
         ]
+        # depth =1
         
     def calculate_tax(self, product:MenuItem):
         return product.price * Decimal(1.1)
+    dci = {
+        "title": "beef steak",
+        "price": "2.50",
+        "stock": 100,
+    }
--- LittlelemonAPI/views.py
+++ LittlelemonAPI/views.py
@@ -1,30 +1,48 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import JsonResponse
-from rest_framework import generics 
+from rest_framework import generics, status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .models import MenuItem  
-from .serializers import MenuItemSerializer 
+from .models import MenuItem , Category
+from .serializers import MenuItemSerializer, CategorySerializer
 
-# Class based views
-class MenuItemsView(generics.ListCreateAPIView):
-    # queryset = MenuItem.objects.select_related('category').all() 
-    queryset = MenuItem.objects.select_related().all() 
-    serializer_class = MenuItemSerializer 
+# # Class based views
+# class MenuItemsView(generics.ListCreateAPIView):
+#     # queryset = MenuItem.objects.select_related('category').all() 
+#     queryset = MenuItem.objects.select_related().all() 
+#     serializer_class = MenuItemSerializer 
     
-class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
-    queryset = MenuItem.objects.all() 
-    serializer_class = MenuItemSerializer
+# class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+#     queryset = MenuItem.objects.all()
+#     serializer_class = MenuItemSerializer
 
-# # Function based views
-# @api_view()
-# def menu_items(request):
-#     items = MenuItem.objects.select_related('category').all() 
-#     serialized_item = MenuItemSerializer(items, many=True)
-#     return (Response(serialized_item.data))
 
-# @api_view()
-# def single_item(request, id):
-#     items = get_object_or_404(MenuItem,pk=id)
-#     serialized_item = MenuItemSerializer(items)
-#     return (Response(serialized_item.data))
+    
+# Function based views
+@api_view(['GET', 'POST'])
+def menu_items(request):
+    if request.method == 'GET':
+        items = MenuItem.objects.select_related('category').all() 
+        serialized_item = MenuItemSerializer(items, many=True)
+        return (Response(serialized_item.data))
+    if request.method == 'POST':
+        serialized_item = MenuItemSerializer(data=request.data)
+        serialized_item.is_valid(raise_exception=True)
+        # serialized_item.validated_data
+        serialized_item.save()
+        return Response(serialized_item.data, status.HTTP_201_CREATED)
+        
+
+@api_view()
+def single_item(request, id):
+    items = get_object_or_404(MenuItem,pk=id)
+    serialized_item = MenuItemSerializer(items)
+    return (Response(serialized_item.data))
+
+@api_view()
+def category_detail(request, pk):
+    category = get_object_or_404(Category, pk=pk) 
+    serialized_category = CategorySerializer(category)
+    return Response(serialized_category.data)
+    
+    
